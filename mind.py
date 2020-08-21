52b43716d4efdd07c40ab0a464bc78c2c1e19e2e
asutegenov <asutegenov@gmail.com>
2020-08-21 08:55:25 +0400

    Критическое обновление

--- mind.py
+++ mind.py
@@ -17,31 +17,22 @@ print("==============================================")
 while True:
   try:
 
-    print("==== 1. Основное задание =====================")
+    print("==== 1. Упражнение 'Цепочка чисел' - Ур. 1 ===")
     print("==== 2. Таблица умножения  ===================")
-    print("==== 3. Упражнения на сложение  ==============")
-    print("==== 4. Упражнения на вычитание  =============")
-    print("==== 5. Упражнения на деление  ===============")
-    print("==== 6. Упражнение 'Цепочка чисел' ===========")
-    print("==== 7. Выход ================================")
+    print("==== 3. Упражнение на сложение ===============") #Будущая цепочка чисел
+    print("==== 4. Выход ================================")
     print("==============================================")
 
     num_question = int(input("========== Выберите пункт из списка: "))
     print("==============================================")
 
     if num_question == 1:
-      mathExercise(iterations, coinTrue, coinFalse, coinSkip, numbers, 5)
-    elif num_question == 2:
       mathExercise(iterations, coinTrue, coinFalse, coinSkip, numbers, 2)
-    elif num_question == 3:
+    elif num_question == 2:
       mathExercise(iterations, coinTrue, coinFalse, coinSkip, numbers, 0)
+    elif num_question == 3:
+      mathExercise(iterations, coinTrue, coinFalse, coinSkip, numbers, 1)
     elif num_question == 4:
-      mathExercise(iterations, coinTrue, coinFalse, coinSkip, numbers, 1)
-    elif num_question == 5:
-      mathExercise(iterations, coinTrue, coinFalse, coinSkip, numbers, 1)
-    elif num_question == 6:
-      mathExercise(iterations, coinTrue, coinFalse, coinSkip, numbers, 4)
-    elif num_question == 7:
       print("================ See you later ===============")
       print("==============================================")
       break
